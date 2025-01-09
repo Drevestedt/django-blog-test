d968c55beb9c25522f3aa8f63ca5a8e1d105a73d
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-01-10 00:05:29 +0100

    Add blog view, update settings for production, and configure URLs

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-308wxim3kond@@(xwc8-9l7%eye7k8fh_&@**pzj-8v2q!$p5u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
